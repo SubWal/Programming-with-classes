b7043bb8ee47a05a5f2a6190b6597158332d426d
Your Name <subodh333@byui.edu>
2024-03-02 18:43:25 -0700

    done

---
+++ Inheritance/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+public class Vehicle
+{
+    public string Make { get; }
+    public string Model { get; }
+
+    public Vehicle(string make, string model)
+    {
+        Make = make;
+        Model = model;
+    }
+
+    public virtual string DisplayInfo()
+    {
+        return $"{Make} {Model}";
+    }
+}
+
+public class Car : Vehicle
+{
+    public int NumDoors { get; }
+
+    public Car(string make, string model, int numDoors) : base(make, model)
+    {
+        NumDoors = numDoors;
+    }
+
+    public override string DisplayInfo()
+    {
+        return $"{base.DisplayInfo()}, {NumDoors} doors";
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Car car = new Car("Toyota", "Corolla", 4);
+        Console.WriteLine(car.DisplayInfo()); // Output: Toyota Corolla, 4 doors
+    }
+}
